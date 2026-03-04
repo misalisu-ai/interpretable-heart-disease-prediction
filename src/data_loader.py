b67a252b47edba5c7afe6540d6c489e6db60be9a
Muhammad Ibrahim Salisu <salisumuhammadibrahim@outlook.com>
2026-03-04 14:44:26 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+COLUMNS = [
+    'age','sex','cp','trestbps','chol','fbs','restecg',
+    'thalach','exang','oldpeak','slope','ca','thal','target'
+]
+
+def load_data(path="data/cleveland.csv"):
+    df = pd.read_csv(path, header=None, names=COLUMNS, na_values='?')
+    return df
