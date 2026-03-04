19ec4575ab49519c06fae6e24eddd9ce5357590a
Muhammad Ibrahim Salisu <salisumuhammadibrahim@outlook.com>
2026-03-04 14:46:37 +0100

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,13 @@
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 
+def scale_data(X_train, X_test):
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled
+
+def polynomial_expand(X_train_scaled, X_test_scaled, degree=2):
+    poly = PolynomialFeatures(degree=degree, include_bias=False)
+    X_train_poly = poly.fit_transform(X_train_scaled)
+    X_test_poly = poly.transform(X_test_scaled)
+    return X_train_poly, X_test_poly
