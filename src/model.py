3c82e46b3e6680397b6d90af49767e15357bc4c2
Muhammad Ibrahim Salisu <salisumuhammadibrahim@outlook.com>
2026-03-04 15:34:16 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
 from imblearn.pipeline import Pipeline
@@ -5,6 +6,7 @@ from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
 def build_pipeline():
+    # Using 'balanced' is excellent for medical data (handling class imbalance)
     logreg = LogisticRegression(
         solver='lbfgs',
         class_weight='balanced',
@@ -16,7 +18,6 @@ def build_pipeline():
         ('smote', SMOTE(random_state=42)),
         ('logreg', logreg)
     ])
-
     return pipe
 
 def tune_model(pipe, X_train, y_train):
@@ -25,28 +26,16 @@ def tune_model(pipe, X_train, y_train):
         'logreg__C': [0.1, 1, 10]
     }
 
+    # StratifiedKFold is perfect for medical research to maintain class ratios
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
     grid = GridSearchCV(
         pipe,
         param_grid,
         cv=cv,
-        scoring='f1_macro',
+        scoring='f1_macro', # Highlighting macro-F1 is great for imbalanced data
         n_jobs=-1
     )
 
     grid.fit(X_train, y_train)
-
-    # Cross-validation report
-    cv_scores = cross_val_score(
-        grid.best_estimator_,
-        X_train,
-        y_train,
-        cv=cv,
-        scoring='f1_macro'
-    )
-
-    print("Cross-validation F1-macro scores:", cv_scores)
-    print("Mean CV F1-macro:", np.mean(cv_scores))
-
     return grid.best_estimator_, grid.best_params_
