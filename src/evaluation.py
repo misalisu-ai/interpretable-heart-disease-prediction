d7fd7326b720e23ab654de8b92a5c0cd71a6d6ca
Muhammad Ibrahim Salisu <salisumuhammadibrahim@outlook.com>
2026-03-04 15:11:31 +0100

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,14 @@
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import numpy as np
 
 def evaluate(model, X_test, y_test):
     y_pred = model.predict(X_test)
+
+    print("\nTest Accuracy:", accuracy_score(y_test, y_pred))
+    print("\nClassification Report:\n")
     print(classification_report(y_test, y_pred))
-    return y_pred, confusion_matrix(y_test, y_pred)
+
+    cm = confusion_matrix(y_test, y_pred)
+    print("\nConfusion Matrix:\n", cm)
+
+    return y_pred, cm
